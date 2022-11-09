ffaaf496b1d68d7396e92c9d0310a0381f9b21e9
ReverseGTLT <108753232+ReverseGTLT@users.noreply.github.com>
2022-11-09 15:42:59 +0200

    upd

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const array = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 let quantity = 0;
 const result = array.reduce((accum, value) => {
-    if (value % 2 === 0) {
+    if (value > 0) {
         quantity++;
         accum += value;
     }
